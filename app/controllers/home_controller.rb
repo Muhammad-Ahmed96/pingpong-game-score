c0ec923ef2077495f5c8b6430fc7536f6899c3c5
Fernando Altuzar <altuzar@gmail.com>
2017-05-06 12:15:24 -0500

    Add Log Game Create

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,4 +10,23 @@ class HomeController < ApplicationController
     @game = Game.new
     @users = User.where.not(id: current_user.id)
   end
+
+  def log_create
+    @opponent = User.find(params[:opponent])
+
+    @game = Game.new({
+      player_1: current_user,
+      player_2: @opponent,
+      player_1_score: params[:game][:player_1_score],
+      player_2_score: params[:game][:player_2_score],
+      date_played: params[:game][:date_played]
+    })
+    if @game.save
+      redirect_to :root, notice: "Game logged successfully"
+    else
+      # ToDo: Set values and proper error notifications
+      redirect_to :log, alert: "Error logging the game: #{@game.errors.messages.to_yaml.gsub("-","")}"
+    end
+  end
+
 end
