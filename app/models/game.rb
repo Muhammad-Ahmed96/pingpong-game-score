99a79f19c8b9155ba7a5c099eae7635e686a3d32
Fernando Altuzar <altuzar@gmail.com>
2017-05-06 12:29:17 -0500

    Add History view

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,21 @@ class Game < ActiveRecord::Base
 
   validates_presence_of :player_1, :player_2, :player_1_score, :player_2_score, :date_played
 
+  def other_player(u)
+    other_player = u == player_2 ? player_1 : player_2
+    other_player.name.to_s.titleize rescue other_player.email.split("@").first.titleize
+  end
+
+  def scores(u)
+    if u == player_1
+      return "#{player_1_score}-#{player_2_score}"
+    else
+      return "#{player_2_score}-#{player_1_score}"
+    end
+  end
+
+  def game_result(u)
+    u == (player_1_score > player_2_score ? player_1 : player_2) ? 'W' : 'L'
+  end
+
 end
