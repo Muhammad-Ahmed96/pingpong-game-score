8dbcb776accd9c7c2b1a7f07afa0c1492282cebf
Fernando Altuzar <altuzar@gmail.com>
2017-05-06 10:57:09 -0500

    Add Games model references

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ActiveRecord::Base
+  belongs_to :player_1, class_name: 'User'
+  belongs_to :player_2, class_name: 'User'
+
+  validates_presence_of :player_1, :player_2, :player_1_score, :player_2_score, :date_played
+
 end
