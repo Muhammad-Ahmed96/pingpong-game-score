af7e78d1c75473de0466caf970b42941cca52347
Fernando Altuzar <altuzar@gmail.com>
2017-05-06 13:11:17 -0500

    Fix Devise controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
-  def getName(u)
+  def get_name(u)
     name = u.name.to_s.length == 0 ? u.email.split("@").first : u.name rescue "¯\_(ツ)_/¯"
     name.titleize
+  end
+
+  def get_name_from_user_id(uid)
+    u = User.find(uid)
+    return get_name(u)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
   has_many :games
 
+  def name_or_email
+    uname = name.to_s.length == 0 ? email.split("@").first : name rescue "¯\_(ツ)_/¯"
+    uname.titleize
+  end
+
   def games
     Game.where('player_1_id = ? OR player_2_id = ?', self.id, self.id)
   end
