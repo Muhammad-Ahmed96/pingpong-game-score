350d9fee6dba6e6cf9e737786b32cdc82d0632a8
Fernando Altuzar <altuzar@gmail.com>
2017-05-06 11:56:10 -0500

    Add Log Game form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @users = User.order("ranking DESC")
+    @users = User.order(ranking: :desc)
   end
 
   def history
   end
 
   def log
+    @game = Game.new
+    @users = User.where.not(id: current_user.id)
   end
 end
